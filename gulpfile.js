c4ebde4eb9955c27c657a93f46cb97203808db5a
Stinni <kristinnf13@ru.is>
2016-02-13 19:09:06 +0000

    added grunt, gulp and angular

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+var source = ['src/**/*.js', '!src/**/*.min.js'];
+
+gulp.task('minify', function () {
+   gulp.src(source)
+      .pipe(uglify())
+      .pipe(gulp.dest('build'))
+});
+
+gulp.task('js', function () {
+   return gulp.src(source)
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'))
+      .pipe(uglify())
+      .pipe(concat('app.min.js'))
+      .pipe(gulp.dest('build'));
+});
