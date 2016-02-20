6d59c4ce620bc8a7c8ec110e048fb660912e8bca
Stinni <kristinnf13@ru.is>
2016-02-20 04:28:00 +0000

    fixed favicon.ico error and route chatroom to roomlist if no param provieded. Chatroom controller now adds person to chatroom

--- client/src/chatroom/ChatRoomController.js
+++ client/src/chatroom/ChatRoomController.js
@@ -1,6 +1,38 @@
 "use strict";
 
-angular.module("chatApp").controller("ChatRoomController", ["$scope", "$http", "ChatResource", "theUser",
-	function ChatRoomController($scope, $http, ChatResource, theUser) {
+angular.module("chatApp").controller("ChatRoomController", ["$scope", "$routeParams", "$location", "ChatResource", "theUser",
+	function ChatRoomController($scope, $routeParams, $location, ChatResource, theUser) {
+	if (!theUser.isLoggedIn) {
+		$location.path("/login");
+		$location.replace();
+	}
+
+	$scope.errorMessage = "";
+	$scope.displayError = false;
+
+	ChatResource.joinRoom({room: $routeParams.roomKey, pass: ""}, function(success, message) {
+		if (!success) {
+			$scope.$apply(function() {
+				$scope.errorMessage = "Joining/Creating room failed.\n The reason the server sends is: " + message;
+				$scope.displayJoinCreateError = true;
+			});
+		} else {
+			$scope.$apply(function() {
+				$scope.errorMessage = "";
+				$scope.displayJoinCreateError = false;
+				$location.path("/chatroom/" + $scope.roomName);
+				$location.replace();
+			});
+		}
+	});
+
+	$scope.onLogout = function onLogout() {
+		ChatResource.logout(function() {
+			theUser.userName = "";
+			theUser.isLoggedIn = false;
+			$location.path("/login");
+			$location.replace();
+		});
+	};
 
 }]);
--- client/src/resource/ChatResource.js
+++ client/src/resource/ChatResource.js
@@ -31,19 +31,12 @@ angular.module("chatApp").factory("ChatResource", function() {
 
 // adduser
 // rooms
-
 // joinroom
-// Should get called when a user wants to join a room. Note that the API supports a password-protected room, however this
-// is optional, i.e. your implementation doesn't have to support this.
-// Parameters:
-// an object containing the following properties: { room: "the id of the room, undefined if the user is creating a new room",
-// pass: "a room password - not required"}
-// a callback function which accepts two parameters:  a boolean parameter, stating if the request was successful or not.
-// and if not (due to password protection or because of something else), the reason why the join wasn't successful.
-// The server responds by emitting the following events: "updateusers" (to all participants in the room), "updatetopic"
-// (to the newly joined user, not required to handle this), "servermessage" with the first parameter set to "join" ( to all
-// participants in the room, informing about the newly added user). If a new room is being created, the message "updatechat"
-// is also emitted. 
+// The server responds by emitting the following events:
+// "updateusers" (to all participants in the room),
+// "updatetopic" (to the newly joined user, not required to handle this),
+// "servermessage" with the first parameter set to "join" ( to all participants in the room, informing about
+// the newly added user). If a new room is being created, the message "updatechat" is also emitted. 
 
 // sendmsg
 // Should get called when a user wants to send a message to a room. 
